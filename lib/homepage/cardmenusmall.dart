624059ddef76828626eb097ff3d15e048fb161a0
JonSnowv2 <dariusandreicoman@gmail.com>
2023-07-31 15:04:14 +0300

    HomePage Finished
    Created HomePage

---
+++ lib/homepage/cardmenusmall.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CardMenuSmall extends StatelessWidget {
+  final IconData icon;
+
+  CardMenuSmall(this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Icon(icon, size: 60, color: Color(0xff1a1a1a),),
+      style: ElevatedButton.styleFrom(
+        shadowColor: Colors.grey,
+        shape: CircleBorder(),
+        padding: EdgeInsets.all(24),
+        backgroundColor: Color(0xff45bac4),
+      ),
+    );
+  }
+}
