06107adaa8392c0bae7a0e7ff32f5438ebcb5ecf
Zdroba Petru <petru.zdroba@gmail.com>
2023-09-19 16:31:13 +0300

    fixed swipe up bug

--- lib/Views/Widgets/foryou.dart
+++ lib/Views/Widgets/foryou.dart
@@ -53,18 +53,18 @@ class _ForYouState extends State<ForYou> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Padding(
-      padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+      padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
       child: GestureDetector(
         onVerticalDragStart: _onVerticalDragStart,
         onVerticalDragUpdate: _onVerticalDragUpdate,
         child: Container(
           width: screenWidth,
           height: 120,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
           ),
-          child: Center(child: Text("For You", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),)),
+          child: const Center(child: Text("For You", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),)),
         ),
       ),
     );
